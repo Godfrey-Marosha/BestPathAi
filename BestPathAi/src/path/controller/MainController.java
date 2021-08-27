1a5c87ccdb9896b7f5aa197fca34760a1fe6d00d
Godfrey-Marosha <62908105+Godfrey-Marosha@users.noreply.github.com>
2021-08-27 15:41:38 +0200

    Update MainController.java

--- BestPathAi/src/path/controller/MainController.java
+++ BestPathAi/src/path/controller/MainController.java
@@ -97,7 +97,7 @@ public class MainController {
             return finalMap;
             *//*for (char c: charArr){
                 System.out.print(c);
-            }*//*
+            }
         }
     }
     */
